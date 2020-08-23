366270ccf549ab436ab8b5ca9e291e3ff0fefe41
ParkIlHoon <chiwoo2074@gmail.com>
2020-08-23 14:51:00 +0900

    UI 추가구현

--- user-ui/src/main/java/com/ddng/userui/canvas/CanvasController.java
+++ user-ui/src/main/java/com/ddng/userui/canvas/CanvasController.java
@@ -1,8 +1,12 @@
 package com.ddng.userui.canvas;
 
+import com.ddng.userui.home.Card;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CanvasController
@@ -10,6 +14,19 @@ public class CanvasController
     @GetMapping("/canvas")
     public String main (Model model)
     {
+        List<Card> cardList = new ArrayList<>();
+
+        for(int idx = 1; idx < 10; idx++)
+        {
+            Card card = Card.builder()
+                    .title("미용 이름" + idx)
+                    .content("미용 설명" + idx)
+                    .thumbnailPath("/images/beauty/work-" + idx + ".jpg")
+                    .build();
+            cardList.add(card);
+        }
+
+        model.addAttribute("cardList", cardList);
         return "canvas/main";
     }
 }
