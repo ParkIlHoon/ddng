5aba7f349ded8ff5a71a92a90fb2cb556457e6e6
ParkIlHoon <chiwoo2074@gmail.com>
2020-09-19 01:07:29 +0900

    UI import

---
+++ admin-ui-bootstrap/src/main/java/com/ddng/adminuibootstrap/AdminUiBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.ddng.adminuibootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminUiBootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminUiBootstrapApplication.class, args);
+    }
+
+}
---
+++ admin-ui-bootstrap/src/test/java/com/ddng/adminuibootstrap/AdminUiBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddng.adminuibootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminUiBootstrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
