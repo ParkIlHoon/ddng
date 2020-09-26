5e0d8e7201e3253c1c3af2f922daf0d3caa20fc8
ParkIlHoon <chiwoo2074@gmail.com>
2020-09-26 18:10:45 +0900

    toast ui calendar 가져오기~

---
+++ admin-ui-bootstrap/src/main/java/com/ddng/adminuibootstrap/controller/ScheduleController.java
@@ -0,0 +1,18 @@
+package com.ddng.adminuibootstrap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/schedule")
+public class ScheduleController
+{
+
+    @GetMapping("/main")
+    public String mainForm (Model model)
+    {
+        return "schedule/main";
+    }
+}
