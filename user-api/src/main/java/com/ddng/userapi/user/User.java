26d3d8d42fdb343a7f9ce675c2e2124862471b51
ParkIlHoon <chiwoo2074@gmail.com>
2020-10-03 17:18:56 +0900

    누락분 추가

---
+++ user-api/src/main/java/com/ddng/userapi/user/User.java
@@ -0,0 +1,33 @@
+package com.ddng.userapi.user;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "USER")
+public class User
+{
+    @Id @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NICKNAME")
+    private String nickName;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "TEL_NO")
+    private String telNo;
+
+    @Column(name = "JOIN_DATE")
+    private LocalDateTime joinDate;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    protected User() { }
+}
