316bad29f8470514b2963ace6f7844de9a431bee
ParkIlHoon <chiwoo2074@gmail.com>
2020-11-05 23:30:43 +0900

    Swagger 적용

--- schedule-api/src/main/java/com/ddng/scheduleapi/schedule/ScheduleType.java
+++ schedule-api/src/main/java/com/ddng/scheduleapi/schedule/ScheduleType.java
@@ -1,11 +1,22 @@
 package com.ddng.scheduleapi.schedule;
 
+import lombok.Getter;
 
+@Getter
 public enum ScheduleType
 {
-    BEAUTY,
-    HOTEL,
-    KINDERGARTEN,
-    VACATION,
-    ETC
+    BEAUTY("미용", "#00a9ff"),
+    HOTEL("호텔", "#ff5583"),
+    KINDERGARTEN("유치원", "#03bd9e"),
+    VACATION("휴가", "#bbdc00"),
+    ETC("기타", "#9d9d9d");
+
+    private String name;
+    private String color;
+
+    ScheduleType(String name, String color)
+    {
+        this.name = name;
+        this.color = color;
+    }
 }
