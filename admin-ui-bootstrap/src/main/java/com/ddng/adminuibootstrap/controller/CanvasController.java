5b991d4405379d5c9d870f24b66161e92a9b3704
ParkIlHoon <chiwoo2074@gmail.com>
2020-11-09 21:34:36 +0900

    디렉터리 변경

---
+++ admin-ui-bootstrap/src/main/java/com/ddng/adminuibootstrap/controller/CanvasController.java
@@ -0,0 +1,16 @@
+package com.ddng.adminuibootstrap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/canvas")
+public class CanvasController
+{
+    @RequestMapping("/main")
+    public String mainForm (Model model)
+    {
+        return "canvas/main";
+    }
+}
