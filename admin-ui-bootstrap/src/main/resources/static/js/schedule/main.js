be88c31af1268764d8645c093cf109328468523d
ParkIlHoon <chiwoo2074@gmail.com>
2020-11-14 18:13:18 +0900

    종류 select2, 고객 select2, 태그 tagify 적용

--- admin-ui-bootstrap/src/main/resources/static/js/schedule/main.js
+++ admin-ui-bootstrap/src/main/resources/static/js/schedule/main.js
@@ -18,6 +18,7 @@
         // 스케쥴 타입 목록 초기화
         initializeScheduleType(data);
 
+        // 스케쥴 팝업 select 용 데이터 가공
         var selectArr = new Array();
         for (var idx = 0; idx < data.length; idx++)
         {
@@ -31,16 +32,35 @@
                 "<span><i class=\"c-icon cil-circle\" style=\"background-color: " + type.color + ";\"></i>&nbsp;&nbsp;" + type.text + "</span>"
             );
             return $type;
-        };
-
-        $("#type-select").select2({
+        }
+        // 스케쥴 팝업 select 구성
+        $("#scheduleType-select").select2({
             placeholder: "스케쥴 종류를 선택해주세요.",
             allowClear: true,
             width : "100%",
             data : selectArr,
             templateResult: formatTypes
         });
-
+        // 스케쥴 팝업 select 구성
+        $("#customer-select").select2({
+            placeholder: "고객을 선택해주세요.",
+            allowClear: true,
+            width : "100%",
+            minimumInputLength : 1,
+            ajax: {
+                url: "http://1hoon.iptime.org:8366/customer-api/customer",
+                method: "GET",
+                data : function (params) {return { keyword: params.term };},
+                processResults: function (data) {
+                    var returnArr = [];
+                    data.content.forEach(customer => returnArr.push({"id" : customer.id, "text" : customer.name}));
+                    return {
+                        results: returnArr
+                    };
+                }
+            }
+        });
+        // 스케쥴 팝업 tagify 구성
         var tagInput = document.querySelector("#tags-input");
         g_tagify = new Tagify(tagInput, {
             pattern: /^.{0,20}$/,
@@ -306,13 +326,24 @@
             var start = event.start ? new Date(event.start.getTime()) : new Date();
             var end = event.end ? new Date(event.end.getTime()) : moment().add(1, 'hours').toDate();
 
+            // 팝업 구성
+            $("#modal-title").text("스케쥴 등록하기");
+            $("#name-input").val("");
+            $("#scheduleType-select").val(null).trigger('change');
+            $("#customer-select").val(null).trigger('change');
+            $("input:checkbox[id='isAllDay-check']").prop("checked", false);
+            $("#stt-date-input").val(moment(start).format("YYYY-MM-DD\THH:mm"));
+            $("#end-date-input").val(moment(end).format("YYYY-MM-DD\THH:mm"));
+            $("#bigo-input").val("");
+
+            // 팝업 오픈
             $('#scheduleModal').modal({
                 show: true
             });
-
-            $("#modal-title").text("스케쥴 등록하기");
-            $("#stt-date-input").val(start);
-            $("#end-date-input").val(end);
+            // 팝업이 닫히면 가이드라인 제거
+            $('#scheduleModal').on('hidden.bs.modal', function (e) {
+                event.guide.clearGuideElement();
+            });
 
             debugger;
         }
