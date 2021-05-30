0f6967ac69c65cc63ab7de85c52e8ecd6b1b0453
박일훈 <chiwoo2074@gmail.com>
2021-05-30 23:29:40 +0900

    feat: 릴리즈 전 세팅
    1. 깨진 테스트 복구
    2. DockerFile 작성

--- customer-api/src/main/java/com/ddng/customerapi/modules/tag/dto/TagDto.java
+++ customer-api/src/main/java/com/ddng/customerapi/modules/tag/dto/TagDto.java
@@ -3,9 +3,11 @@ package com.ddng.customerapi.modules.tag.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @ApiModel("태그 DTO")
+@ToString
 public class TagDto
 {
     @ApiModelProperty(value = "태그", dataType = "String", example = "귀여움")
