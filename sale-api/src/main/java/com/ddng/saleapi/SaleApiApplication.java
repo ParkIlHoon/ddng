f42dd3256fb227f88e79ad5b61053b8b71adb5d6
박일훈 <chiwoo2074@gmail.com>
2021-11-12 01:07:13 +0900

    feat: Feign 기본 세팅
    1. 어노테이션 추가
    2. 설정 Bean 추가

--- sale-api/src/main/java/com/ddng/saleapi/SaleApiApplication.java
+++ sale-api/src/main/java/com/ddng/saleapi/SaleApiApplication.java
@@ -3,13 +3,15 @@ package com.ddng.saleapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SaleApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SaleApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SaleApiApplication.class, args);
+    }
 
 }
